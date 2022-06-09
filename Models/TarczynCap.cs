d93887d866cddb4ee6fc415f115720c6f847c997
PepeKwapien <pppkwapien@gmail.com>
2022-06-09 23:29:05 +0200

    Dodaj pliki projektów.

---
+++ Models/TarczynCap.cs
@@ -0,0 +1,9 @@
+﻿namespace Tarczynews.Models
+{
+    public class TarczynCap
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Message { get; set; }
+    }
+}
