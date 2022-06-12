fa146ab7f5493aa7061eaa82a4470d6f99b5c05c
PepeKwapien <pppkwapien@gmail.com>
2022-06-12 14:46:41 +0200

    Added basuc Crud Views

---
+++ Controllers/CapsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tarczynews.Models;
+
+namespace Tarczynews.Controllers
+{
+    public class CapsController : Controller
+    {
+        IList<TarczynCap> tarczynCaps;
+
+        public CapsController()
+        {
+            tarczynCaps = new List<TarczynCap>();
+        }
+
+        // GET: CapsController
+        public ActionResult Index()
+        {
+            return View(tarczynCaps);
+        }
+
+        // GET: CapsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CapsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CapsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TarczynCap tarczynCap)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CapsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CapsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int id)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CapsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CapsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Models/TarczynCap.cs
+++ Models/TarczynCap.cs
@@ -1,9 +1,15 @@
-﻿namespace Tarczynews.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarczynews.Models
 {
     public class TarczynCap
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public int Number { get; set; }
-        public string Message { get; set; }
+        [Required]
+        public string? City { get; set; }
+        public string? Message { get; set; }
     }
 }
