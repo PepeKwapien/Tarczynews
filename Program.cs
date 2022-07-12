73be2f1372500307a4ac4062d8d37c773d2443cb
PepeKwapien <pppkwapien@gmail.com>
2022-07-12 23:09:48 +0200

    Added pages to login and register

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Tarczynews.Data;
 using Tarczynews.Repositories;
 using Microsoft.AspNetCore.Identity;
+using Tarczynews.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));;
@@ -20,8 +22,8 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 /*builder.Services.AddSingleton<IDataAccess>(da => new TarczynCapContext((new DbContextOptionsBuilder<TarczynCapContext>())
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")).Options));*/
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+/*builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));*/
 
 builder.Services.AddScoped<ITarczynCapRepository, TarczynCapRepository>();
 
@@ -42,8 +44,12 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+});
 
 app.Run();
